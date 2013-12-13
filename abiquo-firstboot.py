ab97975a5d46ef23461ff837739bfd1427286568
Abel Boldú <abel.boldu@abiquo.com>
2013-12-13 17:27:26 +0100

    nfs, api, dc set functions

--- abiquo-firstboot.py
+++ abiquo-firstboot.py
@@ -5,6 +5,8 @@ import sys,os
 import socket
 import signal
 import commands
+import fileinput
+import ConfigParser
 
 
 def signal_handler(signal, frame):
@@ -29,13 +31,40 @@ def check_nfs_url(url):
         pass
     return False
 
-def set_nfs_url(url):
-  if os.path.os.path.exists("/etc/fstab"):
-    # Append nfs url TODO
+def mount_nfs_url(url):
     pass
 
+def set_nfs_url(url):
+  fstab_path = "/etc/fstab"
+  if os.path.exists(fstab_path):
+    # Check this before
+    with open(fstab_path,"r+a") as f:
+      for line in f:
+        if '/opt/vm_repository' in line:
+          return
+      f.write(url+'  /opt/vm_repository nfs    defaults        0 0\n')
+
+def set_api_url(url):
+  conf_path = '/var/www/html/ui/config/client-config.json'
+  if os.path.exists(conf_path):
+    f = fileinput.FileInput(conf_path,inplace=1)
+    for line in f:
+        line = line.replace("http://localhost/api",url)
+        print line.rstrip()
+    f.close()
+
+def set_dc_id(dcid):
+  config = ConfigParser.ConfigParser()
+  conf_path = '/opt/abiquo/config/abiquo.properties'
+  if os.path.exists(conf_path):
+    config.readfp(open(conf_path))
+    config.set('remote-services', 'abiquo.datacenter.id', dcid)
+    config.write(open(conf_path,'wb'))
+
+def set_https():
+    passres
+
 def detect_public_ip():
-  # Try to get the public ip.
   try:
     # Warning: Not working in all linuxes.
     ip = commands.getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:]
@@ -72,7 +101,7 @@ class NfsWindow:
             ButtonChoiceWindow(self.screen,"URL incorrect","Please enter a URL with the form:\n <ip>:<mountpoint>",buttons = ["OK"], width = 50)
         else:
             self.defaulturl = self.entry.value()
-            # Replace nfs in properties TODO
+            set_nfs_url(self.defaulturl)
             return 0
 
 class ApiWindow:
@@ -106,7 +135,7 @@ class ApiWindow:
             self.screen.popWindow()
         else:
             self.defaulturl = self.entry.value()
-            # Replace api in properties / client TODO
+            set_api_url(self.entry.value())
             return 0
 
 class DCWindow:
@@ -116,7 +145,7 @@ class DCWindow:
         self.label = Label('Datacenter name:')
         self.entry = Entry(33,self.defaultdc)
         self.text = TextboxReflowed(50,"Enter Datacenter ID for your set of remote services.\n")
-        self.topgrid = GridForm(self.screen, "API endpoint", 1, 3)
+        self.topgrid = GridForm(self.screen, "Datacenter name", 1, 3)
         self.topgrid.add(self.text,0,0,(0, 0, 0, 1))
         self.grid = Grid(2, 1)
         self.grid.setField (self.label, 0, 0, (0, 0, 1, 0), anchorLeft = 1)
@@ -133,7 +162,23 @@ class DCWindow:
             return -1
         else:
             self.defaultdc = self.entry.value()
-            # Set Datacenter property TODO
+            set_dc_id(self.entry.value())
+            return 0
+
+class HTTPSWindow:
+    def __init__(self,screen):
+        self.screen = screen
+        self.bc = ButtonChoiceWindow(self.screen,"HTTPS ","Do you want to enable secure SSL front-end?",buttons = ["No","Yes"], width = 50)
+
+    def run(self):
+        self.bc.setCurrent(self.entry)
+        result = self.bc.run()
+        rc = self.bb.buttonPressed(result)
+        if rc == "No":
+            return 
+        else:
+            set_https()
+            # Generate certs and set HTTPS
             return 0
 
 # Password is set in anaconda, not needed at firstboot.
@@ -184,6 +229,7 @@ class mainWindow:
             print "Error: Cannot read profiles."
             exit(1)
         else:
+          screen.finish()
           print "Error: No abiquo profiles detected."
           exit(1)
 
@@ -262,11 +308,23 @@ class mainWindow:
               DONE = 1
 
         # Datacenter ID (Server, V2V, Public Cloud, )
+        DONE = 0
+        if ('abiquo-v2v' or 'abiquo-server' or 'abiquo-remote-services' or 'abiquo-public-services' in profiles):
+          # Loop until NFS steps done.
+          while not DONE:
+            self.win = DCWindow(screen)
+            rc = self.win.run()
+            if rc == -1:
+              screen.popWindow()
+              DONE = 1
+            elif rc == 0:
+              screen.popWindow()
+              DONE = 1
+     
         # RS IP (KVM)
-        #  
-
         # SSL Selection
-        
+        # DHCP relay (?)
+        # NFS + Monolithic -> abiquo.appliancemanager.checkMountedRepository = false
         screen.popWindow()
         screen.finish()
         # Also clean terminal
